d0656267d778244a67dd9ecf2b47d627e1c1bcd1
Danyatha Y.K <rnkmdmrnkmdm2@gmail.com>
2026-02-27 13:36:08 +0530

    Update sample dataset for RandomForestRegressor

--- hmm.py
+++ hmm.py
@@ -4,8 +4,8 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 
 # Sample dataset
-x = np.array([[40], [50], [60], [70], [80], [90]])
-y = np.array([45, 55, 65, 75, 85, 95])
+x = np.array([[30], [35], [60], [70], [80], [90]])
+y = np.array([0, 0, 1, 1, 1, 1])
 
 # Create model
 random_forest_reg_model = RandomForestRegressor()
